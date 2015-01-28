0f9a77f55685b09700e0b785082d9f85a1bf224e
jcarapet <jeffcarapetyan@gmail.com>
2015-01-28 13:21:04 -0600

    added case statement and basic installation for postgres ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,30 +9,9 @@
 
 # install the package of postgresql
 
-execute "pull postgres rpm" do 
-	user "root"
-	command "rpm -Uvh http://yum.postgresql.org/9.3/redhat/rhel-5-x86_64/pgdg-centos93-9.3-1.noarch.rpm"
-	action :run
-end
-
-%w{postgresql93-server postgresql93-contrib}.each do |pkg| 
-
-yum_package pkg do
-	action :install
-end
-end
-
-execute "create database" do 
-	user "root" 
-	command "/etc/init.d/postgresql-9.3 initdb" 
-	action :run
-end
-
-service "postgresql-9.3" do 
-	action [:enable, :start]
-end
-
-execute "chkconfig postgresql-9.3 on" do 
-	user "root"
-	action :run
+case node['platform_family']
+when "rhel" 
+	include_recipe "df_postgresql::rhel"
+when "debian"
+	include_recipe "df_postgresql::debian"
 end
